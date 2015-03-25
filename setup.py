a90269c157d86567391b5b15839034c9bf8acef0
extdpahl <a@b>
2015-03-25 14:07:55 +0000

    update setup.py and add hdf5 reader
    [git-p4: depot-paths = "//PYTROLL/dev_EBP/pytroll_packages/dwd_extensions/": change = 287089]

--- setup.py
+++ setup.py
@@ -2,20 +2,26 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
-import sys
 import imp
 
-version = imp.load_source('dwd_extensions.version', 'dwd_extensions/version.py')
+version = imp.load_source('dwd_extensions.version',
+                          'dwd_extensions/version.py')
 
 
 setup(name="dwd_extensions",
       version=version.__version__,
-      description='DWD specific pytroll extensions',
+      description='DWD specific PyTroll extensions',
       author='The EBP team',
       author_email='',
       url="",
-      packages=['dwd_extensions', 'dwd_extensions.mpop', 'dwd_extensions.trollduction'],
-      scripts=['bin/configure.py', 'bin/postprocessor.py', 'bin/supervisor_event_launcher.py'],
+      packages=['dwd_extensions',
+                'dwd_extensions.layout',
+                'dwd_extensions.mipp',
+                'dwd_extensions.mpop',
+                'dwd_extensions.trollduction'],
+      scripts=['bin/configure.py',
+               'bin/postprocessor.py',
+               'bin/supervisor_event_launcher.py'],
       zip_safe=False,
       license="",
       classifiers=[],
