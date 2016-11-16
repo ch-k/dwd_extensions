95ae99263d6c60ad9d35ae93a778402d344fdec9
Christian Kliche <chk@ebp.de>
2016-11-16 11:53:43 +0100

    Added Meteosat8 IODC for world comp + tests

--- dwd_extensions/tests/test_dataset_processors.py
+++ dwd_extensions/tests/test_dataset_processors.py
@@ -22,8 +22,12 @@
 """
 
 import unittest
+import numpy
+from mock import patch
+from datetime import datetime
 import os
 from dwd_extensions.tools.dataset_processors import _create_world_composite
+from dwd_extensions.tools.image_io import read_image
 
 
 class TestWorldComposite(unittest.TestCase):
@@ -36,29 +40,104 @@ class TestWorldComposite(unittest.TestCase):
         dirname = os.path.dirname(__file__)
         self.files = [
             os.path.join(dirname, 'data',
-                         'goes13_IR_107_wcm3km_201604291015.tif'),
+                         'goes13_IR_107_testwcm_201604291015.tif'),
             os.path.join(dirname, 'data',
-                         'goes15_IR_107_wcm3km_201604291015.tif'),
+                         'goes15_IR_107_testwcm_201604291015.tif'),
             os.path.join(dirname, 'data',
-                         'himawari8_IR1_wcm3km_201604291015.tif'),
+                         'himawari8_IR1_testwcm_201604291015.tif'),
             os.path.join(dirname, 'data',
-                         'meteosat7_IR_115_wcm3km_201604291015.tif'),
+                         'meteosat7_IR_115_testwcm_201604291015.tif'),
             os.path.join(dirname, 'data',
-                         'meteosat10_IR_108_wcm3km_201604291015.tif')
+                         'meteosat10_IR_108_testwcm_201604291015.tif')
         ]
 
-    def test_create_world_composite(self):
+    @patch('mpop.projector.get_area_file',
+           return_value=os.path.join(os.path.dirname(__file__),
+                                     'data', 'testareas.def'))
+    def test_create_world_composite(self, get_area_file_function):
         """Test the creation of world composite"""
-        items = [(path, 'wcm3km', '201604291015') for path in self.files]
-        image = _create_world_composite(items)
-        image.show()
+
+        timeslot = datetime(2016, 4, 29, 10, 15)
+        area = 'testwcm'
+
+        lon_limits = {'meteosat10': [-37.5, 28.75],
+                      'meteosat7': [28.75, 83.2],
+                      'himawari8': [83.2, -177.15],
+                      'goes15': [-177.15, -105.],
+                      'goes13': [-105., -37.5]
+                      }
+
+        items = [(path, area, timeslot) for path in self.files]
+        image = _create_world_composite(items, lon_limits, erosion_size=None,
+                                        smooth_width=None)
+        # image.save(os.path.join(os.path.dirname(__file__),
+        #                         'data', 'reference.tif'))
+        # image.show()
+
+        ref_image = read_image(os.path.join(os.path.dirname(__file__),
+                                            'data',
+                                            'reference.tif'),
+                               area, timeslot)
+        # ref_image.show()
 
         self.assertTrue(image is not None)
+        self.assertTrue(_compare_images(image.pil_image(),
+                                        ref_image.pil_image()) == 0)
+
+    @patch('mpop.projector.get_area_file',
+           return_value=os.path.join(os.path.dirname(__file__),
+                                     'data', 'testareas.def'))
+    def test_create_world_composite_smooth(self, get_area_file_function):
+        """Test the creation of world composite with smooth overlaping"""
+
+        timeslot = datetime(2016, 4, 29, 10, 15)
+        area = 'testwcm'
+
+        lon_limits = {'meteosat10': [-37.5, 28.75],
+                      'meteosat7': [20.75, 83.2],
+                      'himawari8': [73.2, -177.15],
+                      'goes15': [-177.15, -105.],
+                      'goes13': [-105., -37.5]
+                      }
+        items = [(path, area, timeslot) for path in self.files]
+        image = _create_world_composite(items, lon_limits)
+        # image.save(os.path.join(os.path.dirname(__file__),
+        #                         'data', 'reference_smooth.tif'))
+        # image.show()
+
+        ref_image = read_image(os.path.join(os.path.dirname(__file__),
+                                            'data',
+                                            'reference_smooth.tif'),
+                               area, timeslot)
+        # ref_image.show()
+
+        self.assertTrue(image is not None)
+        self.assertTrue(_compare_images(image.pil_image(),
+                                        ref_image.pil_image()) == 0)
 
     def tearDown(self):
         """Closing down
         """
         pass
+
+
+def _compare_images(img1, img2):
+    if img1.size != img2.size or img1.getbands() != img2.getbands():
+        return -1
+
+    err_sum = 0
+    if len(img1.getbands()) == 1:
+        data1 = numpy.array(img1.getdata()).reshape(*img1.size)
+        data2 = numpy.array(img2.getdata()).reshape(*img2.size)
+        err_sum += numpy.sum(numpy.abs(data1 - data2))
+    else:
+        for band_index, _ in enumerate(img1.getbands()):
+            data1 = numpy.array([p[band_index]
+                                 for p in img1.getdata()]).reshape(*img1.size)
+            data2 = numpy.array([p[band_index]
+                                 for p in img2.getdata()]).reshape(*img2.size)
+            err_sum += numpy.sum(numpy.abs(data1 - data2))
+    return err_sum
 
 
 def suite():
--- dwd_extensions/tools/dataset_processors.py
+++ dwd_extensions/tools/dataset_processors.py
@@ -4,6 +4,7 @@
 # Author(s):
 
 #   Christian Kliche <chk@ebp.de>
+#   Panu Lahtinen <panu.lahtinen@fmi.fi>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,14 +22,15 @@
 '''This module defines functions to create images out of dataset messages
 '''
 from urlparse import urlparse
-from fnmatch import fnmatch
 from functools import partial
 import logging
 import numpy as np
 import scipy.ndimage as ndi
+from fnmatch import fnmatch
 from mpop.projector import get_area_def
+from pyresample.geometry import AreaDefinition
 from dwd_extensions.tools.image_io import read_image
-import datetime
+from datetime import datetime
 
 LOGGER = logging.getLogger(__name__)
 
@@ -47,67 +49,78 @@ def create_world_composite(msg, proc_func_params):
 
         area = get_area_def(msg.data['area']['name'])
         t_gatherer = msg.data['gatherer_time']
-        if not isinstance(t_gatherer, datetime.datetime):
+        if not isinstance(t_gatherer, datetime):
             try:
-                t_gatherer = datetime.datetime.strptime(
+                t_gatherer = datetime.strptime(
                     t_gatherer, '%Y%m%d%H%M%S')
             except:
                 t_gatherer = None
         items.append((url.path, area, t_gatherer))
 
-    # order images
-    if proc_func_params: 
-        if proc_func_params.has_key('order'):
+    lon_limits = {}
+    erosion_size = None
+    smooth_width = None
+
+    if proc_func_params:
+        # order images
+        if 'order' in proc_func_params:
             order_list = proc_func_params['order'].split('|')
             sort_key = partial(_match_order_index, order_list)
             items = sorted(items, key=sort_key, reverse=True)
-#         for key in proc_func_params.keys:
-#             if key == 'lon_limit':
-#                 vals = 
-            
 
-    lon_limits = None
+        if 'lon_limits' in proc_func_params:
+            sat_lon_list = proc_func_params['lon_limits'].split('|')
+            for sat_lon in sat_lon_list:
+                sat, min_lon, max_lon = sat_lon.split(',')
+                lon_limits[sat] = (float(min_lon), float(max_lon))
 
-    return _create_world_composite(items, lon_limits)
+        if 'erosion_size' in proc_func_params:
+            erosion_size = float(proc_func_params['erosion_size'])
+
+        if 'smooth_width' in proc_func_params:
+            smooth_width = float(proc_func_params['smooth_width'])
+
+    return _create_world_composite(items, lon_limits=lon_limits,
+                                   erosion_size=erosion_size,
+                                   smooth_width=smooth_width)
 
 
 def _match_order_index(order_list, item):
     for idx, pattern in enumerate(order_list):
         if fnmatch(item[0], pattern):
-            print(str(idx) + ' << ' + item[0])
             return idx
-    print(str(len(order_list)) + ' << ' + item[0])
     return len(order_list)
 
 
-def _create_world_composite(items, lon_limits):
-    erosion_size = 20
+def _create_world_composite(items, lon_limits=None,
+                            erosion_size=20,
+                            smooth_width=20):
     # smooth_sigma = 4
-    smooth_width = 20
 
     img = None
     for (path, area, timeslot) in items:
+
+        if not isinstance(area, AreaDefinition):
+            area = get_area_def(area)
 
         next_img = read_image(path, area, timeslot)
 
         if img is None:
             img = next_img
         else:
-            scaled_erosion_size = erosion_size * (float(img.width) / 1000.0)
             # scaled_smooth_sigma = smooth_sigma * (float(img.width) / 1000.0)
-            scaled_smooth_width = smooth_width * (float(img.width) / 1000.0)
 
             img_mask = reduce(np.ma.mask_or,
                               [chn.mask for chn in img.channels])
             next_img_mask = reduce(np.ma.mask_or,
                                    [chn.mask for chn in next_img.channels])
 
-             # Mask overlapping areas away
+            # Mask overlapping areas away
             if lon_limits:
                 for sat in lon_limits:
                     if sat in path:
-                        adef = get_area_def(area)
-                        mask_limits = calc_pixel_mask_limits(adef, lon_limits[sat])
+                        mask_limits = calc_pixel_mask_limits(area,
+                                                             lon_limits[sat])
                         for lim in mask_limits:
                             next_img_mask[:, lim[0]:lim[1]] = 1
                         break
@@ -115,13 +128,23 @@ def _create_world_composite(items, lon_limits):
             alpha = np.ones(next_img_mask.shape, dtype='float')
             alpha[next_img_mask] = 0.0
 
-            # smooth_alpha = ndi.gaussian_filter(
-            #     ndi.grey_erosion(alpha, size=(scaled_erosion_size, scaled_erosion_size)),
-            #        scaled_smooth_sigma)
-            smooth_alpha = ndi.uniform_filter(
-                ndi.grey_erosion(alpha, size=(scaled_erosion_size, scaled_erosion_size)),
-                scaled_smooth_width)
-            smooth_alpha[img_mask] = alpha[img_mask]
+            if erosion_size is not None and smooth_width is not None:
+                scaled_erosion_size = erosion_size * (float(img.width) /
+                                                      1000.0)
+                scaled_smooth_width = smooth_width * (float(img.width) /
+                                                      1000.0)
+
+                # smooth_alpha = ndi.gaussian_filter(
+                #     ndi.grey_erosion(alpha, size=(scaled_erosion_size,
+                #                                   scaled_erosion_size)),
+                #        scaled_smooth_sigma)
+                smooth_alpha = ndi.uniform_filter(
+                    ndi.grey_erosion(alpha, size=(scaled_erosion_size,
+                                                  scaled_erosion_size)),
+                    scaled_smooth_width)
+                smooth_alpha[img_mask] = alpha[img_mask]
+            else:
+                smooth_alpha = alpha
 
             for i in range(0, min(len(img.channels), len(next_img.channels))):
                 chdata = next_img.channels[i].data * smooth_alpha + \
@@ -132,21 +155,17 @@ def _create_world_composite(items, lon_limits):
 
     return img
 
+
 def calc_pixel_mask_limits(adef, lon_limits):
     """Calculate pixel intervals from longitude ranges."""
     # We'll assume global grid from -180 to 180 longitudes
-    scale = 360./adef.shape[1] # degrees per pixel
-    
-    left_limit = int((lon_limits[0] + 180)/scale)
-    right_limit = int((lon_limits[1] + 180)/scale)
+    scale = 360. / adef.shape[1]  # degrees per pixel
+
+    left_limit = int((lon_limits[0] + 180) / scale)
+    right_limit = int((lon_limits[1] + 180) / scale)
 
     # Satellite data spans 180th meridian
     if right_limit < left_limit:
         return [[right_limit, left_limit]]
     else:
         return [[0, left_limit], [right_limit, adef.shape[1]]]
-
-
-# def show(ch):
-#     gimg = geo_image.Image(ch)
-#     gimg.show()
