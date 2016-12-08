4b3879303f8a597cbf36c151e8343cd9ef316ce9
Christian Kliche <chk@ebp.de>
2016-12-08 08:40:59 +0100

    Replaced lxml by standard python xml lib

--- dwd_extensions/sat_incidents/reader.py
+++ dwd_extensions/sat_incidents/reader.py
@@ -21,7 +21,8 @@
 
 '''This module defines a readers for satellite incidents and announcements
 '''
-from lxml import etree
+#from lxml import etree
+import xml.etree.ElementTree as etree
 from dwd_extensions.sat_incidents.repository import Announcement
 from dwd_extensions.sat_incidents.repository import AffectedEntity
 from dwd_extensions.sat_incidents.repository import AnnouncementImpactEnum
